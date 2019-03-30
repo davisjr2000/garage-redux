7eda56abe230bdfcece33d9cd28f5cd4dbecf314
Davis JR <davisjr2000@hotmail.com>
2019-03-30 17:16:39 -0300

    Create fake list of cars

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="garage">
+
+    </div>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -7,20 +7,32 @@ import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
+import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// reducers
+import carsReducer from './reducers/cars_reducer';
+
+const initialState = {
+  garage: prompt("What is your garage name?") || `garage${Math.floor(10 + (Math.random() * 90))}`,
+  cars: []
+}
+
 const reducers = combineReducers({
-  // key: reducer
+  garage: (state = null, action) => state,
+  cars: carsReducer
 });
+
+const store = createStore(reducers, initialState, middlewares);
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <Router history={history}>
       <Switch>
-        TODO
+        <App />
       </Switch>
     </Router>
   </Provider>,
